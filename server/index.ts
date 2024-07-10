86b46fbf8096f9077568c6982b3ba29cb9af6414
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-07-10 23:55:46 +0545

    updated

---
+++ server/index.ts
@@ -0,0 +1,44 @@
+import express from "express"
+import { PrismaClient } from "@prisma/client"
+import dotenv from "dotenv"
+
+// Load environment variables from .env
+dotenv.config()
+
+const app = express()
+const prisma = new PrismaClient()
+
+// Connect to PostgreSQL using Prisma
+async function connectToDatabase() {
+  try {
+    await prisma.$connect()
+    console.log("Connected to PostgreSQL database with Prisma")
+  } catch (error) {
+    console.error("Unable to connect to PostgreSQL database with Prisma", error)
+  }
+}
+
+connectToDatabase()
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+//for creating a new user
+app.post("/api/v1/user", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: "email@gmail.com",
+      name: "John Doe",
+    },
+  })
+  res
+    .status(200)
+    .json({ success: true, message: "User added successfully!", data: user })
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
